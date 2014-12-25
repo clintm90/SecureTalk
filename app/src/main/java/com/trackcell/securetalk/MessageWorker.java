16f9ca7a022e170a3d72490134c08e62278b40d8
Clint Mourlevat <clint.mourlevat@gmail.com>
2014-12-25 01:36:52 +0100

    adding TimerTask in inner class correcting toast issue by putting in Handler

--- app/src/main/java/com/trackcell/securetalk/MessageWorker.java
+++ app/src/main/java/com/trackcell/securetalk/MessageWorker.java
@@ -5,11 +5,19 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
 import android.preference.PreferenceManager;
 import android.widget.Toast;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.UnknownHostException;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -37,37 +45,91 @@ public class MessageWorker extends Service
         mPrefsGlobal = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
 
         mTimer = new Timer();
-        mTimer.scheduleAtFixedRate(MainLoop, Initialize.InitTime, Initialize.ServiceRefreshTime);
+        mTimer.scheduleAtFixedRate(new MainLoop(), Initialize.InitTime, Initialize.ServiceRefreshTime);
 
         return START_STICKY;
     }
 
-    private TimerTask MainLoop = new TimerTask()
+    private final Handler ToastHandler = new Handler()
     {
         @Override
-        public void run()
+        public void handleMessage(Message msg)
         {
-            new Runnable()
-            {
-                @Override
-                public void run()
-                {
-                    Handler handler = new Handler()
-                    {
-                        public void handleMessage(android.os.Message msg)
-                        {
-                            Toast.makeText(getApplicationContext(), "salut", Toast.LENGTH_LONG).show();
-                        }
-                    };
-                    handler.sendEmptyMessage(0);
-                    //Thread.currentThread().setName("MessageWorker_MainLoop");
-                    
-                    /*GetMessageByID CallLoopItem = new GetMessageByID();
-                    CallLoopItem.execute(getIntent().getStringExtra("recipient"), mPrefsGlobal.getString("owner", "none"));*/
-                }
-            };
+            Toast.makeText(getApplicationContext(), "test", Toast.LENGTH_SHORT).show();
         }
     };
+
+    private class GetMessageByID extends AsyncTask<String, Void, String>
+    {
+        protected String doInBackground(String... params)
+        {
+            Thread.currentThread().setName("MessageWorker_GetMessageByID");
+
+            if (params[0].equals("none"))
+            {
+                cancel(true);
+            }
+
+            try
+            {
+                String rts = "", c;
+                URL mURL = new URL(Initialize.SecureTalkServer + "getMessageByID.php?sender=" + "%" + "&recipient=" + params[0] + "&put=false");
+                BufferedReader reader = new BufferedReader(new InputStreamReader(mURL.openStream()));
+                while ((c = reader.readLine()) != null)
+                {
+                    rts += c;
+                }
+                return rts;
+            }
+            catch (UnknownHostException e)
+            {
+                cancel(true);
+                return null;
+            }
+            catch (Exception e)
+            {
+                cancel(true);
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        protected void onCancelled()
+        {
+            //TODO: implement onCancelled()
+        }
+
+        protected void onPostExecute(final String input)
+        {
+            int i = 0;
+            try
+            {
+                JSONObject mRoot = new JSONObject(input);
+                JSONObject mItems = mRoot.getJSONObject("result");
+
+                for (i = 0; i < mItems.getJSONArray("item").length(); i++)
+                {
+                    JSONObject currentObject = mItems.getJSONArray("item").getJSONObject(i);
+
+                    ToastHandler.sendEmptyMessage(0);
+                }
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private class MainLoop extends TimerTask
+    {
+        public void run()
+        {
+            Thread.currentThread().setName("MessageWorker_MainLoop");
+            GetMessageByID CallLoopItem = new GetMessageByID();
+            CallLoopItem.execute(mPrefsGlobal.getString("owner", "none"));
+        }
+    }
 
     @Override
     public void onDestroy()
